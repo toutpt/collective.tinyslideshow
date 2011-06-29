18089c19fe1bf59766414a6531ebd330dddb8923
JeanMichel FRANCOIS aka toutpt <toutpt@gmail.com>
2011-06-29 14:34:57 +0200

    save, doesn t works well in google chrome

--- collective/tinyslideshow/browser/tinyslideshow.py
+++ collective/tinyslideshow/browser/tinyslideshow.py
@@ -7,58 +7,25 @@ from collective.tinyslideshow import i18n
 
 class ITinySlideshowSettings(interface.Interface):
     """The following are the parameters that can be set on the 
-    object more complete documentation will follow soon.
-    Default values are displayed as (10) and
-    recommended values as [1-20].
-    
-    Images
-    ------
-    
-    imgSpeed = int; (10)
-    navOpacity = int; (25)
-    navHover = int; (70)
-    letterbox = “string”; (#000) required color for letterbox on portrait images
-    link = “string”; class name for link hover state
-    
-    Auto Slideshow
-    --------------
-    
-    auto = boolean; (false)
-    speed = int; (10)
-    
-    Information Dialog
-    ------------------
-    
-    info = boolean; (true)
-    infoID = “string”; required for information dialog
-    infoSpeed = int; (10)
-    
-    Thumbnail Slider
-    ----------------
-    
-    thumbs = “string”; id of thumbnail slider, disabled if not set
-    scrollSpeed = int; [1-20] (5)
-    thumbOpacity = int; [0-100] (70)
-    active = “string”; required for thumbnail active border
-    spacing = int; (5) spacing between thumbnails
-    left = “string”; id of left navigation link, required for slider
-    right = “string”; id of right navigation link, required for slider
-
+    object
     """
     #Image
     imgSpeed   = schema.Int(title=i18n.imgSpeed,
                             default=10)
     navOpacity = schema.Int(title=i18n.navOpacity,
-                            default=25)
+                            default=25)# TODO force [0-100]
     navHover   = schema.Int(title=i18n.navHover,
                             default=70)
+
     #Scroll
     auto  = schema.Bool(title=i18n.auto,
                        default=True)
     speed = schema.Int(title=i18n.speed,
                        default=10)
-    #Thumbnail
 
+    #Thumbnail
+    info        = schema.Bool(title=i18n.info,
+                              default=True)
     infoSpeed   = schema.Int(title=i18n.infoSpeed,
                              default=10)
     scrollSpeed = schema.Int(title=i18n.scrollSpeed,
