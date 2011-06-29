ec5d1565171804ebd55be9ecb22499ed45274306
JeanMichel FRANCOIS aka toutpt <toutpt@gmail.com>
2011-06-29 11:46:34 +0200

    initial commit

---
+++ collective/tinyslideshow/interfaces/__init__.py
@@ -0,0 +1,2 @@
+from tinyslideshowlayerlayer import ITinySlideshowLayer
+
---
+++ collective/tinyslideshow/interfaces/tinyslideshowlayerlayer.py
@@ -0,0 +1,8 @@
+from zope.interface import Interface
+# -*- Additional Imports Here -*-
+
+
+class ITinySlideshowLayer(Interface):
+    """ A layer specific to this product. 
+        Is registered using browserlayer.xml
+    """
